5e67878854f501db6c5ac7a4615ed9fe4429ee9d
Benedek Rozemberczki <benedek.rozemberczki@gmail.com>
2019-03-08 15:56:15 +0000

    Update appnp.py

--- src/appnp.py
+++ src/appnp.py
@@ -2,11 +2,10 @@ import torch
 import random
 import numpy as np
 from tqdm import trange, tqdm
-from utils import create_propagator_matrix
-from appnp_layer import APPNPModel
-
 from torch_sparse import spmm
 from texttable import Texttable
+from appnp_layer import APPNPModel
+from utils import create_propagator_matrix
 
 class APPNPTrainer(object):
 
